328569d46ce9a7388144ef9416528015edfac34b
Remya8Roy <remya08roy@gmail.com>
2018-01-12 16:03:46 +0530

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flyworld
+//
+//  Created by DDU  kaushalkendra on 1/11/18.
+//  Copyright © 2018 DDU  kaushalkendra. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
